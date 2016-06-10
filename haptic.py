db025ac19fd17e3ec7a9cf27f1dd66de22dfd085
leicas <wspeed@gmail.com>
2016-06-10 16:01:00 +0200

    Working with bidirectionnal communication

--- haptic.py
+++ haptic.py
@@ -4,8 +4,9 @@ from pyqtgraph.Qt import QtGui, QtCore
 import pyqtgraph as pg
 import pyqtgraph.widgets.RemoteGraphicsView
 from pylibftdi import Device
-
+import numpy as np
 app = pg.mkQApp()
+resang = 100
 
 pg.setConfigOptions(antialias=True)  ## this will be expensive for the local plot
 
@@ -24,10 +25,11 @@ lcheck = QtGui.QCheckBox('plot local')
 lcheck.setChecked(True)
 lplt = pg.PlotWidget()
 lplt.setYRange(-45,45)
-lplt.setTitle('Temperature')
+lplt.setTitle('Position')
 fplt = pg.PlotWidget()
-fplt.setYRange(0,255)
+fplt.setYRange(-150,150)
 fplt.setTitle('Forces')
+fplt.getAxis('bottom').setScale(1.0/resang)
 layout = pg.LayoutWidget()
 layout.addWidget(labelf)
 layout.addWidget(labelp)
@@ -40,13 +42,26 @@ layout.addWidget(label, row=3, col =1)
 layout.addWidget(lplt, row=4, col=0, colspan=3)
 layout.addWidget(fplt, row=5, col=0, colspan=3)
 layout.resize(800,800)
-layout.setWindowTitle('pyqtgraph example: RemoteSpeedTest')
+layout.setWindowTitle('Timon 12: Demo')
 layout.show()
 
 ## Create a PlotItem in the remote process that will be displayed locally
 lastUpdate = pg.ptime.time()
 data=[0]*1000
-force1=[0]*100
+anglemax = 45
+resang = 100
+force=[0]*2*anglemax*resang
+for num in range(0, 2*anglemax*resang):
+    if num < anglemax*resang:
+        if num < anglemax*resang/2:
+            force[num]=(anglemax*resang-num*2)*1.0/resang
+        else:
+            force[num]=0
+    else:
+        if num > anglemax*resang + anglemax*resang/2:
+            force[num]=(anglemax*resang-num+anglemax*resang/2)*2.0/resang
+        else:
+            force[num]=0
 force2=[0]*100
 force3=[0]*100
 avgFps = 0.0
@@ -56,13 +71,13 @@ buffdata = [0]*100
 iterfiltre = 0
 dev = Device()
 dev.baudrate = 230400
-count = 100
+count = 10
 i = 0
 def update():
-    global dev, i, count, label, avgFps, lastUpdate, plt, data
-    rec = bytearray(dev.read(3))
-    if len(rec) >= 3:
-        if rec[0] != 5 or rec[1] == 5:
+    global dev, i, count, label, avgFps, lastUpdate, plt, data, force, anglemax, resang
+    rec = bytearray(dev.read(6))
+    if len(rec) >= 6:
+        if rec[0] != 5 or rec[3] != 5:
             rec = bytearray(dev.read(2))
             while(len(rec) < 1):
                   rec = bytearray(dev.read(1))
@@ -70,9 +85,9 @@ def update():
                 rec = bytearray(dev.read(1))
                 while(len(rec) < 1):
                   rec = bytearray(dev.read(1))
-            if len(rec) < 3:
-                rec[1:2] = dev.read(2)
-        if len(rec) == 3 and rec[1] != 5:
+            if len(rec) < 6:
+                rec[1:5] = dev.read(5)
+        if len(rec) == 6 and rec[3] == 5:
          i+=1
          angle = rec[1] + rec[2] * 256
          if angle > 32767:
@@ -81,17 +96,27 @@ def update():
          data[:-1] = data[1:]
          data[-1] = degre
          #print(str(degre))
+         indexf = max(min(int((anglemax+degre)*resang),anglemax*resang*2-1),0)
+         forcenow=force[indexf]
+         forcenowint = 32767*(1+forcenow/130)
+         bufenvoi = bytearray(4)
+         bufenvoi[0] = int(forcenowint) & int('0b00111111',2)
+         bufenvoi[1] = ((int(forcenowint) >> 6) & int('0b00111111',2)) | int('0b01000000',2)
+         bufenvoi[2] = ((int(forcenowint) >> 12) & int('0b00111111',2)) | int('0b10000000',2)
+         bufenvoi[3] = int('0b11000000',2)
+         dev.write(bufenvoi)
          if i>= count:
             lplt.plot(data, clear=True)
-            print(str(degre))
+            #print(str(degre))
             #print(str(degre) + " | " +str(rec[0]) + " : " + str(rec[1]) + " : " +str(rec[2]))
             i=0
             #print(str(count/(end-start)))
-    now = pg.ptime.time()
-    fps = 1.0 / (now - lastUpdate)
-    lastUpdate = now
-    avgFps = avgFps * 0.8 + fps * 0.2
-    label.setText("Generating %0.2f fps" % avgFps)
+            now = pg.ptime.time()
+            fps = count / (now - lastUpdate)
+            lastUpdate = now
+            label.setText("Communication %0.2f Hz" % fps)
+            fplt.plot(range(-anglemax*resang,anglemax*resang),force, clear=True)
+            fplt.plot([degre*resang],[forcenow],pen=(0,0,255),symbolBrush=(255,0,0),symbolPen='r')
 timer = QtCore.QTimer()
 timer.timeout.connect(update)
 timer.start(0)
