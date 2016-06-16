a12fb19eee4cd8e5ef1a36a1698519c83acc8ef7
leicas <wspeed@gmail.com>
2016-06-16 11:20:43 +0200

    Add unittest and travis fonction

---
+++ test_haptic.py
@@ -0,0 +1,16 @@
+import unittest
+import haptic
+import multiprocessing
+
+class TestFonctionUtile(unittest.TestCase):
+    def test_extract(self):
+        fifo = multiprocessing.Queue()
+        size = 5
+        fifoverif = bytearray(range(0,size))
+        for i in range(0,size):
+            fifo.put(bytes([i]))
+            #fifoverif[i]=bytes([i])
+        fifotest = haptic.extract(fifo,size)
+        self.assertEqual(fifotest,fifoverif)
+if __name__ == '__main__':
+    unittest.main()
