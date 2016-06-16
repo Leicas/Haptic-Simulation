b4e022a82481c8bfe8612d8cbd5455d0fce19ac7
leicas <wspeed@gmail.com>
2016-06-16 16:08:04 +0200

    Cleanup of the code

--- haptic.py
+++ haptic.py
@@ -1,3 +1,4 @@
+""" Module for Communication and Ploting of Force """
 import os
 import time
 from threading import Thread
@@ -6,8 +7,12 @@ import pyqtgraph as pg
 from pylibftdi import Device
 import multiprocessing
 
-# Classe de lecture
+RESANG = 100
+COUNT = 100
+ANGLEMAX = 45
+
 def lecture(fifo):
+    """ Read the device informations """
     dev = Device()
     dev.baudrate = 230400
     while True:
@@ -16,21 +21,23 @@ def lecture(fifo):
         except (KeyboardInterrupt, SystemExit):
             print ("Exiting lecture...")
             break
-
 def extract(fifo, size):
+    """ Extract 'size' bytes from 'fifo' and return a bytearray """
     rec = bytearray([0]*size)
     for i in range(0,size):
         rec[i] = int.from_bytes(fifo.get(), 'big')
     return rec
-            
+
 def compute(threadname):
-    global fifo, i, fps, count, label, avgFps, plt, shared, Force, Anglemax, Resang, Taille
+    """ Compute the force """
+    global fps, label, avgFps, plt, shared, Force, ANGLEMAX, Taille
     dev = Device()
     dev.baudrate = 230400
-    lastUpdate = pg.ptime.time()
+    lastupdate = pg.ptime.time()
+    i = 0
     while True:
         Taille = fifo.qsize()
-        if(Taille>=3):
+        if Taille>=3:
             rec = bytearray(extract(fifo,3))
             Taille = Taille - 3
             if rec[0] != 5:
@@ -43,13 +50,13 @@ def compute(threadname):
                 i+=1
                 angle = rec[1] + rec[2] * 256
                 if angle > 32767:
-                   angle -= 65536
+                    angle -= 65536
                 degre = angle*360/20000
                 data = shared['data']
                 data[:-1] = data[1:]
                 data[-1] = degre
                 shared['data'] = data
-                indexf = max(min(int((Anglemax+degre)*Resang),Anglemax*Resang*2-1),0)
+                indexf = max(min(int((ANGLEMAX+degre)*RESANG),ANGLEMAX*RESANG*2-1),0)
                 forcenow=Force[indexf]
                 forcenow = max(min(forcenow,130),-130)
                 forcenowint = 32767*(1+forcenow/130)
@@ -60,67 +67,16 @@ def compute(threadname):
                 bufenvoi[1] = ((int(forcenowint) >> 6) & int('0b00111111',2)) | int('0b01000000',2)
                 bufenvoi[2] = ((int(forcenowint) >> 12) & int('0b00111111',2)) | int('0b10000000',2)
                 bufenvoi[3] = int('0b11000000',2)
-                if i>= count:
+                if i>= COUNT:
                     i= 0
                     now = pg.ptime.time()
-                    shared['fps'] = count / (now - lastUpdate)
+                    shared['fps'] = COUNT / (now - lastupdate)
                     shared['Taille'] = Taille
-                    lastUpdate = now
+                    lastupdate = now
                 dev.write(bufenvoi)
 
-Resang = 100
-
-
-## Create a PlotItem in the remote process that will be displayed locally
-Anglemax = 45
-Resang = 100
-degre = 0
-forcenow = 0
-Taille = 0
-Force=[0]*2*Anglemax*Resang
-for num in range(0, 2*Anglemax*Resang):
-    if num < Anglemax*Resang:
-        if num < Anglemax*Resang/2:
-            Force[num]=(Anglemax*Resang-num*2)*4.0/Resang
-        else:
-            Force[num]=0
-    else:
-        if num > Anglemax*Resang + Anglemax*Resang/2:
-            Force[num]=(Anglemax*Resang-num+Anglemax*Resang/2)*8.0/Resang
-        else:
-            Force[num]=0
-
-force3=[0]*100
-fps = multiprocessing.Value('d',0.0)
-itera = 0
-oldforce = 0
-buffdata = [0]*100
-iterfiltre = 0
-#data=[0]*1000
-
-count = 100
-i = 0
-
-
-
-class Graph(pg.QtCore.QThread):
-    newData = pg.QtCore.Signal()
-    def __init__(self):
-        super(Graph, self).__init__()
-        self.stopMutex = Lock()
-        self._stop = False
-    def run(self):
-        while True:
-            with self.stopMutex:
-                if self._stop:
-                    break
-            self.newData.emit()
-            time.sleep(0.2)
-    def stop(self):
-        with self.stopMutex:
-            self._stop = True
-
-def Affichage(name, shared):
+def affichage(name, shared):
+    """ Ploting and Display """
     pg.mkQApp()
     pg.setConfigOptions(antialias=True)  ## this will be expensive for the local plot
     force = pg.SpinBox(value=0,int=True,minStep=1,step=10,bounds=(-128,128))#QtGui.QLineEdit()
@@ -143,7 +99,7 @@ def Affichage(name, shared):
     fplt = pg.PlotWidget()
     fplt.setYRange(-150,150)
     fplt.setTitle('Forces')
-    fplt.getAxis('bottom').setScale(1.0/Resang)
+    fplt.getAxis('bottom').setScale(1.0/RESANG)
     layout = pg.LayoutWidget()
     layout.addWidget(labelf)
     layout.addWidget(labelp)
@@ -160,48 +116,61 @@ def Affichage(name, shared):
     layout.show()
     #data = shared['data']
     def update(shared):
+        """ Every refresh of the display """
         localdata=[0]*1000
-        Taille = shared['Taille']
+        taille = shared['Taille']
         localdata=shared['data']
         #for i in range(0,1000):
         #    localdata[i] = shared['data'][i]
         lplt.plot(localdata, clear=True)
         fps = shared['fps']
-        label.setText("Communication %0.2f Hz Taille buffer: %0.2f" % (fps, Taille/3.0))
-        Anglemax = shared['anglemax']
-        Resang = shared['Resang']
-        Force = shared['force']
+        label.setText("Communication %0.2f Hz Taille buffer: %0.2f" % (fps, taille/3.0))
+        force = shared['force']
         degre = shared['degre']
         forcenow = shared['forcenow']
-        fplt.plot(range(-Anglemax*Resang,Anglemax*Resang),Force, clear=True)
-        fplt.plot([degre*Resang],[forcenow],pen=(0,0,255),symbolBrush=(255,0,0),symbolPen='r')
+        fplt.plot(range(-ANGLEMAX*RESANG,ANGLEMAX*RESANG),force, clear=True)
+        fplt.plot([degre*RESANG],[forcenow],pen=(0,0,255),symbolBrush=(255,0,0),symbolPen='r')
     timer = QtCore.QTimer()
     timer.timeout.connect(lambda: update(shared))
     timer.start(50)
     QtGui.QApplication.instance().exec_()
 
-    
 if __name__ == '__main__':
     fifo = multiprocessing.Queue()
     manager = multiprocessing.Manager()
     shared = manager.dict()
     data = manager.list()
     data = [0] * 1000
+    degre = 0
+    forcenow = 0
+    Taille = 0
+    fps = multiprocessing.Value('d',0.0)
+    Force=[0]*2*ANGLEMAX*RESANG
+    for num in range(0, 2*ANGLEMAX*RESANG):
+        if num < ANGLEMAX*RESANG:
+            if num < ANGLEMAX*RESANG/2:
+                Force[num]=(ANGLEMAX*RESANG-num*2)*4.0/RESANG
+            else:
+                Force[num]=0
+        else:
+            if num > ANGLEMAX*RESANG + ANGLEMAX*RESANG/2:
+                Force[num]=(ANGLEMAX*RESANG-num+ANGLEMAX*RESANG/2)*8.0/RESANG
+            else:
+                Force[num]=0
     shared['data'] = data
     shared['fps'] = 0.0
-    shared['anglemax'] = Anglemax
-    shared['Resang'] = Resang
+    shared['ANGLEMAX'] = ANGLEMAX
+    shared['RESANG'] = RESANG
     shared['force'] = Force
     shared['degre'] = degre
     shared['forcenow'] = forcenow
-    ##lecture = Thread(target=lecture, args=("Thread-1", ) )
     compute = Thread(target=compute, args=("Thread-2", ) )
     lecturep = multiprocessing.Process(target=lecture, args=(fifo,))
     lecturep.start()
     time.sleep(0.5)
     print(fifo.get())
     compute.start()
-    affp = multiprocessing.Process(target=Affichage, args=("test",shared,))
+    affp = multiprocessing.Process(target=affichage, args=("test",shared,))
     affp.start()
     while True:
         try:
@@ -218,9 +187,6 @@ if __name__ == '__main__':
             print ("Exiting...")
             os._exit(0)
             break
-##    graph = Graph()
-##    graph.newData.connect(update)
-##    graph.start()
 
         
 
